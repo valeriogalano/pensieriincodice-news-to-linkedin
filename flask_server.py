ddc05fd764b5d9e48b7ce7208f296c856f13fbd3
progressify <a.porcelli90@gmail.com>
2024-10-01 16:56:31 +0200

    server flask

---
+++ flask_server.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import json
+from os import getenv
+
+from flask import Flask, request
+
+app = Flask(__name__)
+CALLBACK_URL_PORT = getenv("CALLBACK_URL_PORT", 8000)
+
+
+@app.route('/', methods=['GET'])
+def callback():
+    code = request.args.get('code')
+    today_date = datetime.now().strftime("%Y-%m-%d")
+
+    with open(f'{today_date}.json', 'a') as f:
+        json_string = json.dumps({'code': code})
+        f.write(json_string)
+
+    return 'Codice memorizzato! Attendi la chiusura del browser...'
+
+
+def run_server():
+    app.run(host='0.0.0.0', port=CALLBACK_URL_PORT)
