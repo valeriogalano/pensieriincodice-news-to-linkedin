da4f45a2d6be26d1e018dabd86476d08982629a9
progressify <a.porcelli90@gmail.com>
2024-10-02 10:09:29 +0200

    aggiorno il main per testare il flusso di autenticazione

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-
 from linkedin_helper import LinkedinHelper
 
 if __name__ == "__main__":
     linkedin = LinkedinHelper()
-    linkedin.start_local_server()
-    time.sleep(120)
-    linkedin.stop_local_server()
+    linkedin.auth()
